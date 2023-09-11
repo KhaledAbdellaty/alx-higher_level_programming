f3839437fa67a79c8a23d74ed5fb8c087a23a0c5
KhaledAbdellaty <kabdellaty@gmail.com>
2023-09-11 05:25:37 +0000

    task 2

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    import sys
+    argv = sys.argv[1:]
+    argc = len(argv)
+    print("{} {}{}".format(
+         argc,
+	 "argument" if argc == 1 else "arguments",
+	 ":" if argc != 0 else "."
+	 ))
+    if argc > 0:
+	for i in range(argc):
+	    print("{}: {}".format(i + 1, argv[i]))
