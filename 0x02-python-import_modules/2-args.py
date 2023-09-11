2391d033d1f74b870545de3f60d61d03c957cf46
KhaledAbdellaty <kabdellaty@gmail.com>
2023-09-11 05:34:51 +0000

    task 2

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -5,9 +5,9 @@ if __name__ == "__main__":
     argc = len(argv)
     print("{} {}{}".format(
          argc,
-	 "argument" if argc == 1 else "arguments",
-	 ":" if argc != 0 else "."
-	 ))
+         "argument" if argc == 1 else "arguments",
+         ":" if argc != 0 else "."
+         ))
     if argc > 0:
         for i in range(argc):
             print("{}: {}".format(i + 1, argv[i]))
