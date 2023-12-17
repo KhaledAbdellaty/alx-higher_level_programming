25e31d111f8aea68f4f92877bff3557c206b7af6
KhaledAbdellaty <kabdellaty@gmail.com>
2023-12-17 11:16:57 -0800

    0. Get all states

---
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""a script that lists all states from the database hbtn_0e_0_usa."""
+import MySQLdb
+import sys
+
+if __name__ == "__main__":
+    conn = MySQLdb.connect(host="localhost", port=3306, user=sys.argv[1],
+                           passwd=sys.argv[2], db=sys.argv[3])
+
+    curs = conn.cursor()
+    curs.execute("SELECT * FROM states ORDER BY id ASC")
+    query_row = curs.fetchall()
+    for row in query_row:
+        print(row)
+
+    curs.close()
+    conn.close()
---
+++ 0x0F-python-object_relational_mapping/README.md
@@ -0,0 +1,1 @@
+This project is about 0x0F. Python - Object-relational mapping.
